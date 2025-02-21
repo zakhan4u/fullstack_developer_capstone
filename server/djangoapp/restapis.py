94d973bdb4d1f18d87b84a9ada5eaac0486c3a73
YourGitHubUsername <YourGitHubEmail>
2025-02-21 15:02:08 +0500

    ninth

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -12,6 +12,22 @@ sentiment_analyzer_url = os.getenv(
     default="http://localhost:5050/")
 
 # def get_request(endpoint, **kwargs):
+def get_request(endpoint, **kwargs):
+    params = ""
+    if(kwargs):
+        for key,value in kwargs.items():
+            params=params+key+"="+value+"&"
+
+    request_url = backend_url+endpoint+"?"+params
+
+    print("GET from {} ".format(request_url))
+    try:
+        # Call get method of requests library with URL and parameters
+        response = requests.get(request_url)
+        return response.json()
+    except:
+        # If any error occurs
+        print("Network exception occurred")
 # Add code for get requests to back end
 
 # def analyze_review_sentiments(text):
